76938834934dc6d3252a8ba540e357e45361c2e8
kavyaspandanamandadi <spandana.kavya415@gmail.com>
2017-06-05 01:49:39 -0400

    This is my file to create Autoscaling

---
+++ autoscaling.tf
@@ -0,0 +1,49 @@
+resource "aws_elb" "web-elb" {
+  name = "sample-elb"
+
+  availability_zones = ["${split(",", var.availability_zones)}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+}
+
+resource "aws_autoscaling_group" "web-asg" {
+  availability_zones   = ["${split(",", var.availability_zones)}"]
+  name                 = "example-asg"
+  max_size             = "${var.asg_max}"
+  min_size             = "${var.asg_min}"
+  desired_capacity     = "${var.asg_desired}"
+  force_delete         = true
+  launch_configuration = "${aws_launch_configuration.web-lc.name}"
+  load_balancers       = ["${aws_elb.web-elb.name}"]
+
+  #vpc_zone_identifier = ["${split(",", var.availability_zones)}"]
+  tag {
+    key                 = "Name"
+    value               = "web-asg"
+    propagate_at_launch = "true"
+  }
+}
+
+resource "aws_launch_configuration" "web-lc" {
+  name          = "example-lc"
+  image_id      = "${var.aws_amis}"
+  instance_type = "${var.instance_type}"
+
+  # Security group
+  security_groups = ["${var.sgid}"]
+#  user_data       = "${file("userdata.sh")}"  ##simple shell script with echo message inline testing
+  key_name        = "${var.key_name}"
+}
